f503879b598ed645d93c48f4ceced45966b17a69
linda@reachmaine.com <linda@reachmaine.com>
2018-06-26 11:31:49 -0400

    first pass at new child theme.

---
+++ downeast-calendar/functions.php
@@ -0,0 +1,11 @@
+<?php
+
+  require_once(get_stylesheet_directory().'/custom/branding.php');
+  require_once(get_stylesheet_directory().'/custom/language.php');
+  require_once(get_stylesheet_directory().'/custom/reach_CTAs.php');
+  require_once(get_stylesheet_directory().'/custom/oshine.php');
+
+
+  //enqueue the init script for broadstreet ads
+	wp_enqueue_script( 'broadstreet', '//cdn.broadstreetads.com/init.js');
+?>
