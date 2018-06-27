c77710a543e69726c80a8fcdd83008393a41bfcc
linda@reachmaine.com <linda@reachmaine.com>
2018-06-27 15:33:03 -0400

    playing with events
    get tribe events to look as we like

--- downeast-calendar/functions.php
+++ downeast-calendar/functions.php
@@ -4,7 +4,7 @@
   require_once(get_stylesheet_directory().'/custom/language.php');
   require_once(get_stylesheet_directory().'/custom/reach_CTAs.php');
   require_once(get_stylesheet_directory().'/custom/oshine.php');
-
+  require_once(get_stylesheet_directory().'/custom/tribe_events.php');
 
   //enqueue the init script for broadstreet ads
 	wp_enqueue_script( 'broadstreet', '//cdn.broadstreetads.com/init.js');
